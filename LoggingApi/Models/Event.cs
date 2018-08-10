463896acdd32f547fa597de9474b80e838ee9e00
Daniel Przybylski <DanielP@meyer.com>
2018-08-10 12:39:48 -0700

    Front end basically working.

--- LoggingApi/Models/Event.cs
+++ LoggingApi/Models/Event.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Meyer.Logging.Models
 {
 	public class Event
 	{
+		[Required(AllowEmptyStrings = false)]
 		public string Description { get; set; }
+		[Required(AllowEmptyStrings = false)]
 		public string EnvironmentName { get; set; }
+		[Required(AllowEmptyStrings = false)]
 		public string ApplicationName { get; set; }
+		[Required(AllowEmptyStrings = false)]
 		public string TypeName { get; set; }
-		public DateTime TimeStamp { get; set; }
+		[Required]
+		public DateTime TimeStamp { get; set; } = DateTime.Now;
 	}
 }
