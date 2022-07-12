2b9838af303765c9774ad17c46a1220c0d24f797
Daniel Przybylski <danielp@meyer.com>
2022-07-11 21:02:08 -0700

    Recreate functions project

---
+++ Functions (copy)/EntryItem.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Meyer.Logging
+{
+	public class EntryItem
+	{
+		public string ClientApplicationName { get; set; }
+		public string Severity { get; set; }
+		public string UserId { get; set; }
+		public JObject Entry { get; set; }
+	}
+}
